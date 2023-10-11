c16565de363a1cade074472c02bf3e7dbff4d8be
VicCalderoon <146136453+VicCalderoon@users.noreply.github.com>
2023-10-11 13:52:44 -0700

    Agregar archivos de proyecto.

---
+++ 3KLSC-VICCALDERON-PRACT06-C SHARP/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vectores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> aves = new List<string> { "Loro gris, ", "Paloma de diamante, ", "Cóctel, " };
+            Console.WriteLine("Los valores de la lista antes de insertar: ");
+            foreach (string ave in aves)
+            {
+                Console.Write(ave + " ");
+            }
+        }
+    }
+}
